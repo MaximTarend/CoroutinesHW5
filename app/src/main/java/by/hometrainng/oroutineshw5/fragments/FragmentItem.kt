ddf424657f808d83dc04b5c6195c00041c76be55
Maxim Tarend <84102129+MaximTarend@users.noreply.github.com>
2022-05-09 17:03:53 +0300

    Layouts + Navigation

---
+++ app/src/main/java/by/hometrainng/oroutineshw5/fragments/FragmentItem.kt
@@ -0,0 +1,39 @@
+package by.hometrainng.oroutineshw5.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import by.hometrainng.oroutineshw5.databinding.FragmentItemBinding
+
+class FragmentItem: Fragment() {
+
+    private var _binding: FragmentItemBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentItemBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun showToastMessage(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/by/hometrainng/oroutineshw5/fragments/FragmentList.kt
@@ -0,0 +1,41 @@
+package by.hometrainng.oroutineshw5.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import by.hometrainng.oroutineshw5.databinding.FragmentListBinding
+
+
+class FragmentList : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentListBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    private fun showToastMessage(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
